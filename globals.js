173589e520e91e8128922a22986255dafb52ca22
swiftmas <collinhamel@gmail.com>
2017-05-25 15:40:30 -0700

    added more npcs

--- globals.js
+++ globals.js
@@ -3,7 +3,7 @@
 
 exports = module.exports = {};
 exports.collmap = require("./coll.json");
-exports.coredata = {"players":{}, "dest":{}, "effects":[], "attacks":[], "npcs":{"n001":{"pos":"70.50","state":"000","health":100,"alerttimer":0,"team":"04","skin":"redKnight","dir":"2","origin":"70.40"}}};
+exports.coredata = {"players":{}, "dest":{}, "effects":[], "attacks":[], "npcs":{"n001":{"pos":"70.50","state":"000","health":100,"alerttimer":0,"team":"04","skin":"redKnight","dir":"2","origin":"70.40"},"n002":{"pos":"160.55","state":"000","health":100,"alerttimer":0,"team":"04","skin":"redKnight","dir":"2","origin":"150.55"}}};
 exports.mapchange = false;
 exports.attackQueue = {};
 exports.movementQueue = {};
